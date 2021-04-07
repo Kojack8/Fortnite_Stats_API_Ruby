5c53e57dd1385934e46662b5f7ed4f4041246e30
Kojack8 <72641704+Kojack8@users.noreply.github.com>
2021-04-07 12:15:59 -0500

    Form correctly fetches player by user name

--- app/services/tracker.rb
+++ app/services/tracker.rb
@@ -1,7 +1,7 @@
 module Tracker
   class Search
-    def self.by_location(location)
-      Faraday.get 'https://fortnite-api.com/v1/stats/br/v2/ec60c981982d4ab4951d27bc6171d93c'
+    def self.by_location(user)
+      Faraday.get 'https://fortnite-api.com/v1/stats/br/v2/?name=' + user
     end
   end
 end
